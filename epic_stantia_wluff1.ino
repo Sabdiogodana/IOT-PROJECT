67e1dea63ad604365d6b0ce430813d365ab0ad04
Sabdiogodana <99467959+Sabdiogodana@users.noreply.github.com>
2022-03-11 19:18:57 +0300

    Add files via upload

---
+++ epic_stantia_wluff1.ino
@@ -0,0 +1,27 @@
+// C++ code
+int Red = 1;
+int Green = 2;
+int Yellow = 0;
+//
+void setup()
+{
+  pinMode(Red, OUTPUT);
+  pinMode(Green, OUTPUT);
+  pinMode(Yellow, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(1, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(1, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(2, HIGH);
+  delay(1000);
+  digitalWrite(2, LOW);
+  delay(1000);
+  digitalWrite(0, HIGH);
+  delay(1000);
+  digitalWrite(0, LOW);
+  delay(1000);
+}
